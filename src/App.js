c906290a397acc19aa5bc0c27faefc13e119cad4
Zubair Farooqui <zubairfarooqui.here@gmail.com>
2023-12-17 12:59:37 +0530

    add dark mode basics

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+
+//import { Toggle } from "./components/Toogle";
 
 function App() {
+  const [isDark, setIsDark] = useState(false);
+
   return (
+    // <div className="App" data-theme={isDark ? "dark" : "light"}>
+    //   <Toggle isChecked={isDark} handleChange={() => setIsDark(!isDark)} />
+    //   <h1 className="title">Hello world!</h1>
+    //   <div className="box">
+    //     <h2>This is a box</h2>
+    //   </div>
+    // </div>
     <>
-      <div id="content">
-        <header>Header</header>
-
-        <main>Main</main>
-
-        <aside>Aside</aside>
-
-        <nav>Nav</nav>
-
-        <footer>Footer</footer>
+      <div className="bd" data-theme={true ? "dark" : "light"}>
+        <div className="container">
+          <div className="containerOne">
+            <h2>Hello Dark Mode</h2>
+          </div>
+          <div className="containerTwo">
+            <p>Let's switch color</p>
+          </div>
+        </div>
       </div>
     </>
   );
